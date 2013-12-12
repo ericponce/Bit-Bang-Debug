c9df23331aa4999459a017d6f55dae03114f8333
narango <slickarango@gmail.com>
2013-12-11 20:28:31 -0500

    fixed all the the things

--- firmware/uart_drv.h
+++ firmware/uart_drv.h
@@ -2,17 +2,19 @@
 #include <avr/interrupt.h>
 
 #define UART_PORT_IN PORTB
-#define UART_PORT_OUT DDRB
+#define UART_PORT_OUT PORTB
 #define UART_PIN_IN PB0
 #define UART_PIN_OUT PB1
 
-#define uart_set_up() {TCCR1 |= (1 << CTC1); MCUCR |= (1 << ISC01);} //Clear on overflow, external interupt on falling edge
+//#define uart_set_up() {TCCR1 |= (1 << CTC1); MCUCR |= (1 << ISC01);} //Clear on overflow, external interupt on falling edge
+#define uart_set_up() TCCR1 |= ((1 << CTC1) | (1 << CS10)) //Clear on overflow, external interupt on falling edge
+
 #define uart_tear_down() (TCCR1 &= ~(1 << CTC1))
-#define uart_set_baud(bd) (OCR1C = bd)
+#define uart_set_baud(bd) {OCR1C = bd; OCR1A = bd;}
 #define uart_reset_baud() (OCR1C = 0)
 
-#define uart_start_timing() (TIMSK |= (1 << TOIE1)) //Enable TImer 1 Interrupt
-#define uart_stop_timing() (TIMSK &= ~(1 << TOIE1))
+#define uart_start_timing() (TIMSK |= (1 << OCIE1A)) //Enable TImer 1 Interrupt
+#define uart_stop_timing() (TIMSK &= ~(1 << OCIE1A))
 #define uart_reset_timing() (TCNT1 = 0)
 
 #define uart_set_high() (UART_PORT_OUT |= (1 << UART_PIN_OUT))
